88b160a94167685510033a3f29f9bcb5869094f9
Ola-B <mailto.obra@gmail.com>
2021-07-10 22:18:18 +0200

    auto_commit

--- pir.py
+++ pir.py
@@ -2,18 +2,30 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 import subprocess
+import os
  
 SENSOR_PIN = 4
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
 
+def image_files():
+    path = '/home/pi/git/github/motion_triggered_cam/images'
+    files = os.listdir(path)
+    file_html = open("/home/pi/git/github/motion_triggered_cam/index.html", "w")
+    for f in files:
+        file_html.writelines(f+"\n")
+        print(f)
+    file_html.close()
 
 def motion(SENSOR_PIN):
     # Here, alternatively, an application / command etc. can be started.
-    print("There was a movement!",str(datetime.datetime.now()))
+    filename = str(datetime.datetime.now())+".jpg"
+    print("There was a movement! New file: ",filename)
     rc = subprocess.call("./webcam.sh")
-    time.sleep(10)
+    time.sleep(2)
+    image_files()
+    
     rc = subprocess.call("./git_push.sh")
 
 print("PIR setup...")
