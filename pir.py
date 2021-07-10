58e89bf33ec5184c4fd0542c0428a0ba226c0a56
Ola-B <mailto.obra@gmail.com>
2021-07-10 18:12:43 +0200

    first commit

---
+++ pir.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+import datetime
+import subprocess
+ 
+SENSOR_PIN = 4
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SENSOR_PIN, GPIO.IN)
+
+
+def motion(SENSOR_PIN):
+    # Here, alternatively, an application / command etc. can be started.
+    print("There was a movement!",str(datetime.datetime.now()))
+    rc = subprocess.call("./webcam.sh")
+
+print("PIR setup...")
+time.sleep(2)
+print("Ready")
+
+try:
+    GPIO.add_event_detect(SENSOR_PIN , GPIO.RISING, callback=motion)
+    while 1:
+        time.sleep(500)
+except KeyboardInterrupt:
+    print("Finish...")
+    GPIO.cleanup()
+    
+    
---
+++ webcam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DATE=$(date +"%Y-%m-%dT%H:%M:%S")
+
+fswebcam -r 1280x720 /home/pi/git/github/motion_triggered_cam/images/$DATE.jpg
