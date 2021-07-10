1803e206cfab99c2e95fa74b8f1f2002627f1899
Ola-B <mailto.obra@gmail.com>
2021-07-10 22:38:33 +0200

    auto_commit

--- pir.py
+++ pir.py
@@ -12,6 +12,8 @@ SENSOR_PIN = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
 
+#https://github.com/Ola-B/motion_triggered_cam/blob/main/images/2021-07-10T22:26:20.jpg?raw=true
+
 def image_files():
     path = '/home/pi/git/github/motion_triggered_cam/images'
     files = os.listdir(path)
@@ -21,7 +23,7 @@ def image_files():
     file_html.writelines("<body>")
 
     for f in files:
-        url = '"https://github.com/Ola-B/motion_triggered_cam/tree/main/images/'+f+'"'
+        url = '"https://github.com/Ola-B/motion_triggered_cam/tree/main/images/'+f+'?raw=true"'
         file_html.writelines("<img src="+url+" alt='Cat'>\n")
         print(f)
     
